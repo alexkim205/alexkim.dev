9f510b6d01a81c12639f352e1661d034a22549ae
Alex Kim <alexgkim205@gmail.com>
2021-04-18 22:17:39 -0700

    temlpate change

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
+import { TwitterTweetEmbed as Tweet } from "react-twitter-embed";
 
 import Bio from "../components/bio";
 import Layout from "../components/layout";
@@ -29,7 +30,7 @@ class BlogPostTemplate extends React.Component {
         >
           {post.frontmatter.date}
         </p>
-        <MDXRenderer>{post.body}</MDXRenderer>
+        <MDXRenderer shortcodes={{ Tweet }}>{post.body}</MDXRenderer>
         <hr
           style={{
             marginBottom: rhythm(1),
